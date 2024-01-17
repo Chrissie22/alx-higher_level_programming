056d4d47c7d34fd5903c1bc528c2702b6220ce0e
Chrissie <christabelojobolo@yahoo.com>
2024-01-17 08:17:38 -0800

    Prints numbers from 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,7 +2,7 @@
 """Prints numbers from 0 to 99"""
 
 for i in range(100):
-        if i == 99:
-            print("{}".format(i))
-        else:
-            print("{:02}".format(i), end=", ")
+    if i == 99:
+        print("{}".format(i))
+    else:
+        print("{:02}".format(i), end=", ")
