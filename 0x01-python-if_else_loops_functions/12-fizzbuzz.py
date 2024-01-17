f59c8620ccc6f5f5edd898bec68efbb65e202785
Chrissie <christabelojobolo@yahoo.com>
2024-01-17 08:51:08 -0800

    Prints numbers from 1 to 100 and substitute them accordingly

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 # Author - Christabel Ojobolo
-"""
-    Print the numbers from 1 to 100 seperated by a spcae.
+"""Print the numbers from 1 to 100 seperated by a spcae.
     For multiples of three, print Fizz instead of the number
     For multiples of five, print Buzz instead of the number
-    For multiples of three and five, print FizzBuzz instead of the number.
-"""
+    For multiples of three and five, print FizzBuzz instead of the number."""
 
 
 
 def fizzbuzz():
-
-
     for number in range(1, 101):
         if number % 3 == 0 and number % 5 == 0:
             print("FizzBuzz ", end="")
