e456304cbd346934c28db252562b5e64ed4ac3de
Chrissie <christabelojobolo@yahoo.com>
2024-01-17 07:15:58 -0800

    Checks if a list have a loop

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * check_cycle - Checks if there s loop in a single linked list
+ * @list: Linked list to check
+ *
+ * Return: 0 if there is no loop, otherwise 1.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_y *slow, *fast;
+
+	slow = list;
+	fast = list;
+	while (slow && fast && fast->next)
+	{
+		if (slow == fast)
+			return (1);
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return (0);
+}
