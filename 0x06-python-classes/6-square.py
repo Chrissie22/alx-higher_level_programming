72310aebffeb2581a08eaef45963fb0040aecf57
Chrissie <christabelojobolo@yahoo.com>
2024-02-07 08:04:03 -0800

    Added position attribute and modified my_print method

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -33,7 +33,7 @@ class Square:
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
                 not all(isinstance(i, int) for i in value) or
-                all(n < 0 for n in value)):
+                not all(n >= 0 for n in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
@@ -45,9 +45,10 @@ class Square:
         """Prints in stdout the square with the character '#'"""
         if self.__size == 0:
             print()
-        else:
-            [print() for i in range(0, int(self.__position[1]))]
-            for i in range(0, int(self.__size)):
-                [print(' ', end='') for j in range(0, int(self.__position[0]))]
-                [print('#', end='') for k in range(0, int(self.__size))]
-                print()
+            return
+        
+        [print() for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(' ', end='') for j in range(0, self.__position[0])]
+            [print('#', end='') for k in range(0, self.__size)]
+            print()
