86e2775109e676164e550a30f5f3bde7fa02edae
Chrissie <christabelojobolo@yahoo.com>
2024-02-07 05:13:08 -0800

    Added a private attribute __size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a Square class"""
+
+
+class Square:
+    """A Square class"""
+    
+    def __init__(self, size):
+        """Initializes a new square."""
+        self.__size = size
